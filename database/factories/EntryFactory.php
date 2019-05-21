33f5a69f0d910cbae8a56e925e3c3752ce2f7751
hassehulabeck <hansbudandersson@gmail.com>
2019-05-21 20:05:24 +0200

    Added models, r-controllers and factories

---
+++ database/factories/EntryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Entry;
+use Faker\Generator as Faker;
+
+$factory->define(Entry::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
