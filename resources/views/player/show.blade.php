827cb25779c0fbe8643e4d01041ec4b27b8c2eac
hassehulabeck <hansbudandersson@gmail.com>
2019-05-22 20:39:12 +0200

    Fixed teamlink on player.show

--- resources/views/player/show.blade.php
+++ resources/views/player/show.blade.php
@@ -6,7 +6,9 @@
         <h3>
             <span class="flag-icon flag-icon-{{$player->team->abbreviation }} ">
             </span>
-            {{ $player->team->name }}
+            <a href="/teams/{{$player->team->id}}"> 
+                {{ $player->team->name }}
+            </a>
         </h3>
         <p>{{ $player->points }} poäng hittills.</p>
     </div> 
