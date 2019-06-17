e64e03ef4fdc099b8a4904db07bc3b3a3ab3371d
hassehulabeck <hansbudandersson@gmail.com>
2019-06-17 10:43:20 +0200

    added null matches

--- resources/views/match/index.blade.php
+++ resources/views/match/index.blade.php
@@ -11,6 +11,7 @@
             </div>
                     
             <div class="col-6 col-md-4 offset-md-1">
+                @if ($match->homeTeam[0] != null)
                 {{$match->homeTeam[0]->name}}
                 <span class="flag-icon flag-icon-{{$match->homeTeam[0]->abbreviation }} ">
                     </span>
@@ -30,6 +31,10 @@
                 </h4>                                            
                 @endif
             </div>
+                    
+                @else
+                    Lag ej klara                    
+                @endif
             @can('admin-only')
                 @if ($match->home_goals === null)
                     
